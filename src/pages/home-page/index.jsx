e315b16c3dcda83c995a65b589692d7596cb32c5
Aditya Singh <118240130+ThisIsMeAdityaSingh@users.noreply.github.com>
2024-12-19 12:51:45 +0000

    Reformatted code

--- src/pages/home-page/index.jsx
+++ src/pages/home-page/index.jsx
@@ -1,7 +1,7 @@
-import "./App.css";
-import { GradientHoverButton } from "./components/gradient-hover-button";
+import { GradientHoverButton } from "../../components/gradient-hover-button";
+import "./styles.css";
 
-export default function App(){
+export default function HomePage(){
   return (
     <main className="hp-main-container">
       <header className="hp-header-container">
@@ -9,7 +9,6 @@ export default function App(){
           <ul>
             <li>View your past designs</li>
             <li>View open sourced designes</li>
-            <li>Build something new</li>
           </ul>
         </section>
         <section>
@@ -21,8 +20,8 @@ export default function App(){
           <center>Design Your Dream Outfit.</center>
           <center>Share Your Style, Inspire the World</center>
         </div>
-        <center className="sub-text">{`Design your dream outfit with AI. Tell us how you want it to look, and we'll make it happen 😊 Love your design? 
-          Open source it for the world to see or order your personalized piece.`}</center>
+        <center className="sub-text">Design your dream outfit with AI. Tell us how you want it to look, and we'll make it happen 😊 Love your design? 
+          Open source it for the world to see or order your personalized piece.</center>
         <section className="hp-button-group">
           <GradientHoverButton text={"Start Designing Now"} />
           <GradientHoverButton theme="light" text={"Explore Our Templates"} />
